8c6082182948e5f8da789309fca2e41df74cedca
Agussstin13 <agusgromo2000@hotmail.com>
2024-10-23 09:06:37 -0300

    estructura de carpetas

---
+++ FINAL-ALGORITMOS/src/Routes.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/*" element={<Login />}></Route>
+          <Routes path="/">
+
+          </Routes>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
